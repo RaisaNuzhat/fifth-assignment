469895648e528d4b7394638c0612aabccf8ba2fa
RaisaNuzhat <raisa.csecu@gmail.com>
2024-02-19 19:00:43 +0600

    scrolling done

---
+++ app.js
@@ -0,0 +1,11 @@
+
+function scrollToSelect()
+{
+    const scrollTarget=document.getElementById("scrollToSelect");
+    scrollTarget.scrollIntoView({behavior:"smooth"});
+}
+document.getElementById("scrollButton").addEventListener("click",scrollToSelect);
+function selectButton()
+{
+    const selectedButton=document.getElementById("")
+}
